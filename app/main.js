9d3b34f5449858f39398b5d2d938a66424f7bdee
Juti Buican <jbuican19@gmail.com>
2021-04-21 22:57:05 +0100

    First commit

---
+++ app/main.js
@@ -0,0 +1,7 @@
+const Backbone = require('backbone');
+module.exports = function () {
+  return Backbone;
+};
+// https://github.com/pipefishbook/ch_1
+// https://github.com/pipefishbook
+// use command browserify -r ./app/main:app > static/bundle.js
