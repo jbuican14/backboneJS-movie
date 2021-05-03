c5803669792e536d5f31e2f228632c0d5daab663
Juti Buican <jbuican19@gmail.com>
2021-05-03 16:40:27 +0100

    Updated main.js

--- app/main.js
+++ app/main.js
@@ -2,6 +2,7 @@ const Backbone = require('backbone');
 const Movies = require('collections/movies');
 const data = require('../movies.json');
 const Monitor = require('./monitor');
+const MoviesList = require('views/moviewsList');
 
 // module.exports = function () {
 //   return Backbone;
@@ -9,4 +10,8 @@ const Monitor = require('./monitor');
 const movies = new Movies(data);
 const monitor = new Monitor(movies);
 
-module.exports = movies;
+module.exports = {
+  movies: movies,
+  MovieView: this.MovieView,
+  MoviesList: MoviesList,
+};
