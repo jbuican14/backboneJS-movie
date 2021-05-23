3a7403111c3130766d0681cd4ff8ee4ddb83ec84
Juti Buican <jbuican19@gmail.com>
2021-05-23 22:14:32 +0100

    Advanced templates: eco and JST

--- app/views/genresFilter.js
+++ app/views/genresFilter.js
@@ -1,11 +1,13 @@
 const Backbone = require('backbone');
-const genresTemplate = require('../templates/genres.jst');
+// const genresTemplate = require('../templates/genres.jst');
+// for eco
+const genresTemplate = require('../templates/genres.eco');
 
 // the UI for selecting a Movie Category
 const GenresView = Backbone.View.extend({
   template: genresTemplate,
 
-  rener: function () {
+  render: function () {
     this.$el.html(this.template({ genres: this.genres }));
     return this;
   },
